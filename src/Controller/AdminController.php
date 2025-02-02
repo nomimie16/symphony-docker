555884bfe38e24a6c83b59f75a3cefe6d43e9b94
Noemie L <831noemie@gmail.com>
2025-02-02 22:50:14 +0100

    admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -27,9 +27,9 @@ final class AdminController extends AbstractController
     #[Route('/admin/test1', name: 'app_admin_test1')]
     public function index_test(): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
         // or add an optional message - seen by developers
-        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN', null, 'User tried to access a
         page without having ROLE_ADMIN');
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
